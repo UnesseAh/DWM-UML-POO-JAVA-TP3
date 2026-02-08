d3bcb10c64aa56a7eadc4c495b42ecc14a9a3016
UnesseAh <youness.ahasla@gmail.com>
2026-02-08 18:33:43 +0100

    create Paypal

---
+++ Exercice8/PayPal.java
@@ -0,0 +1,15 @@
+package Exercice8;
+
+public class PayPal extends Paiement {
+    private String email;
+
+    public PayPal(double montant, int numeroTransaction, String email) {
+        super(montant, numeroTransaction);
+        this.email = email;
+    }
+
+    @Override
+    public void effectuerPaiement(double montant) {
+        System.out.println("Paiement de " + montant + " effectue par PayPal (Email: " + email + ").");
+    }
+}
