72801f1616aa99c9965b60b1619542eb4782265f
UnesseAh <youness.ahasla@gmail.com>
2026-02-08 18:19:51 +0100

    create Figure

---
+++ Exercice6/Figure.java
@@ -0,0 +1,19 @@
+package Exercice6;
+
+public abstract class Figure {
+    protected String nom;
+
+    public Figure(String nom) {
+        this.nom = nom;
+    }
+
+    public abstract double calculerAire();
+
+    public abstract double calculerPerimetre();
+
+    public void afficherDetails() {
+        System.out.println("Figure: " + nom +
+                ", Aire: " + calculerAire() +
+                ", Perimetre: " + calculerPerimetre());
+    }
+}
