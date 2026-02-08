d6daf1ced32b5dfc066daf8e8d402665b3ce7696
UnesseAh <youness.ahasla@gmail.com>
2026-02-08 17:04:23 +0100

    create vehicule

---
+++ Exercice4/Vehicule.java
@@ -0,0 +1,19 @@
+package Exercice4;
+
+public class Vehicule {
+    protected String nom;
+    protected double prix;
+
+    public Vehicule(String nom, double prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public void emettreSon() {
+        System.out.println("Le vehicule emet un son inconnu.");
+    }
+
+    public void afficherInformations() {
+        System.out.println("Nom: " + nom + ", Prix: " + prix);
+    }
+}
