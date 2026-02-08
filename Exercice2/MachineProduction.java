a917875b32f30b67b1f0af5406edabac4ba29c01
UnesseAh <youness.ahasla@gmail.com>
2026-02-08 16:38:31 +0100

    create MachineProduction and remove TestCompte class

---
+++ Exercice2/MachineProduction.java
@@ -0,0 +1,35 @@
+package Exercice2;
+
+public class MachineProduction {
+    private int id;
+    private int nombreProduits;
+
+    private static int prochainId = 1;
+    private static int totalProduits = 0;
+    private static int plafondProduction = 0;
+
+    public MachineProduction() {
+        this.id = prochainId++;
+        this.nombreProduits = 0;
+    }
+
+    public static void setPlafondProduction(int plafond) {
+        plafondProduction = plafond;
+    }
+
+    public static int getTotalProduits() {
+        return totalProduits;
+    }
+
+    public boolean produire(int quantite) {
+        if (totalProduits + quantite <= plafondProduction) {
+            this.nombreProduits += quantite;
+            totalProduits += quantite;
+            System.out.println("Machine " + id + " a produit " + quantite + " objets.");
+            return true;
+        } else {
+            System.out.println("Plafond de production atteint pour la machine " + id);
+            return false;
+        }
+    }
+}
